69fc76a965badd86aa5a81acfb534be6fdad1267
Kyle Gersbach <39284048+GersbachKa@users.noreply.github.com>
2020-01-09 15:10:42 -0800

    Added comments

--- HW1.m
+++ HW1.m
@@ -1,26 +1,37 @@
+%Set variables
 m = 9.11 * (10^-31);
 E = 10;
 V = 9;
+
+%Constants
 hbar = 6.582 * (10^-16);
 
+%Values based on constants and variables
 k1 = sqrt(2*m*E)/hbar;
 k2 = sqrt(2*m*(E-V))/hbar;
 
+%Probability calculations
 T = (4*k1*k2)/((k1+k2)^2);
 R = ((k1-k2)/(k1+k2))^2;
 
+%Output calculated values
 disp("Given m="+m+" kg, E="+E+" eV, V="+V+" eV")
 disp("Transmision probability: "+T+", and Reflection probability: "+R)
 
 disp(" ")
 %--------------------------------------------------------------------------
+%Stick game
+
+%An array of 100 children, all initially at -1 or no stick. If a child has
+%a 1, the child has a stick.
+
 children = -1*ones(100,1);
 
 for basket = 1:100
-    current = basket;
-    while current<=100
-        children(current) = children(current)*-1;
-        current=((current/basket)+1)*basket;
+    current = basket; %Current child with the basket
+    while current<=100 %Exit statement when counting to children above 100 
+        children(current) = children(current)*-1; %Swap status of current
+        current=((current/basket)+1)*basket; %Go to next multiple
     end
 end
 
