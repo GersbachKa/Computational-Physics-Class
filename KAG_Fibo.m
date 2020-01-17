4c2246ccfe460cfbe949c2f4f1acac6284451d66
Kyle Gersbach <39284048+GersbachKa@users.noreply.github.com>
2020-01-16 20:44:32 -0800

    "Functions in Matlab"

---
+++ KAG_Fibo.m
@@ -0,0 +1,26 @@
+﻿% Kyle Gersbach
+% 1/16/2020
+
+
+function fibs = Fibo(n)
+    fibs = [0,1];
+    if n>2 && floor(n) == n && abs(n)~=inf && isnumeric(n)
+        for i = 3:n+1
+            fibs(i) = fibs(i-1) + fibs(i-2);
+        end
+        
+    elseif n == 2
+        fibs = [0,1,1];
+    
+    elseif n == 1
+        fibs = [0,1];
+    
+    elseif n == 0
+        fibs = [0];
+    
+    else
+        disp("The number must be a positive whole number.");
+        fibs = [];
+        
+    end
+end
---
+++ KAG_PythagoreanTriples.m
@@ -0,0 +1,24 @@
+﻿% Kyle Gersbach
+% 1/16/2020
+
+function triples = KAG_PythagoreanTriples(n)
+    triples = [];
+    for a = 1:n
+        for b = a:n
+            c = sqrt(a^2 + b^2);
+            if floor(c)==c
+                
+                %Now check others to see if it is a multiple
+                if isempty(triples)
+                    triples(size(triples)(1)+1,:) = [a,b,c];
+                    disp([a,b,c])
+                else
+                    if gcd(a,b)==1
+                        triples(size(triples)(1)+1,:) = [a,b,c];
+                        disp([a,b,c])
+                    end
+                end
+            end
+        end
+    end
+end
