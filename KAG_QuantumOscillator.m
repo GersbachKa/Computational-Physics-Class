eea54d5c0e2085a74620bbecbae054ad72a49132
Kyle Gersbach <39284048+GersbachKa@users.noreply.github.com>
2020-02-06 14:31:20 -0800

    Update KAG_QuantumOscillator.m

--- KAG_QuantumOscillator.m
+++ KAG_QuantumOscillator.m
@@ -1,30 +1,40 @@
 function KAG_QuantumOscillator()
-    global v0 hbar a m E
-    v0=50;
-    hbar = 197*1e-9;
-    m=.511*1e6;
-    E=413;
-    a=1.e-11;
+    % set some parameters
+    global V0 a hbar m E
+    V0= 50;
+    a=1.e-11; % m
+    hbar=197*1e-9; % hbar-c
+    m=0.511*1e6; % eV/c2
+    E=413; % eV Energy level
     
     %initial conditions
+    psi0=0;
+    psip0=1;
+    
     xMin = -10*a;
     xMax = 10*a;
     
-    [] = ode45(@Rate,[xMin,xMax],[])
     
+    
+    [x,pout] = ode45(@Rate,[xMin,xMax],[psi0;psip0]);
+    psi = pout(:,1);
+    
+    plot(x,psi);
+    xlim([-5*a,5*a])
 end
 
 function r=Rate(x,P)
+    global V0 a hbar m E
+
+    % unpack
     psi = P(1);
     psip = P(2);
-    
+        
+        
+    % compute rates
     dpsi = psip;
-    dpsip = -1*(hbar/(2*m))*(E-potential(x))*psi;
-    
-    r = [dpsi;dpsip];
-end
-
-function v=potential(x)
-    global v0 a
-    v=v0.*((x^.2)./(a^2));
+    dpsip = -(2*m/hbar^2)*(E - V0*(x^2/a^2))*psi;
+        
+    % pack rates into column vector
+    r = [dpsi; dpsip];
 end
